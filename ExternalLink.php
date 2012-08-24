7e0cb4b5d52d94889a46a0b81936a9fbc0420bb2
Nick Freear <nfreear@yahoo.co.uk>
2012-08-24 11:39:14 +0100

    Adding support for multiple links, via Piwik [ExternalLink] configuration..
    * ..Also, the lack of 'links["http://example.org"] = "LABEL"' doesn't cause fatal errors!

--- ExternalLink.php
+++ ExternalLink.php
@@ -15,8 +15,9 @@
  Example plugin configuration -- config/config.ini.php
 
 [ExternalLink]
-label = "Track OER Home"
-url = "http://track.olnet.org"
+links["http://track.olnet.org"] = "Project Home"
+links["http://blog.olnet.org"] = "Blog"
+
 
 */
 
@@ -31,11 +32,12 @@ class Piwik_ExternalLink extends Piwik_Plugin
 		return array(
 			'description' =>
 			'* Add a link to the Piwik site top menu, pointing to an external site [JISC Track OER project]',
-			#Piwik_Translate('CoreAdminHome_PluginDescription'),
+			#'__description' => Piwik_Translate('ExternalLink_PluginDescription'),
 			'homepage' => 'http://track.olnet.org/',
 			'author' => 'IET at The Open University',
 			'author_homepage' => 'http://iet.open.ac.uk/',
 			'version' => '0.1',
+			#'translationAvailable' => true,
 		);
 	}
 
@@ -43,7 +45,6 @@ class Piwik_ExternalLink extends Piwik_Plugin
 	{
 		return array(
 			'TopMenu.add' => 'addTopMenu',
-			#'AdminMenu.add' => 'addMenu',
 			'AssetManager.getCssFiles' => 'getCssFiles',
 		);
 	}
@@ -51,25 +52,19 @@ class Piwik_ExternalLink extends Piwik_Plugin
 
 	public function addTopMenu()
 	{
-		$label = Piwik_Config::getInstance()->ExternalLink['label'];
-		//if (! $label) $label = 'Home';
+		$links = $this->getConfigLinks();
 
-		//try {
-		$url = Piwik_Config::getInstance()->ExternalLink['url'];
-		/*} catch($e) {
-		var_dump($e);
-		echo 'Exception..'
-		$url = '';
-		}*/
-
-		Piwik_AddTopMenu($label,
-			array('module' => 'ExternalLink', 'action' => 'redirect',
-				'url' => $url),
+		foreach ($links as $url => $label)
+		{
+			Piwik_AddTopMenu($label,
+				array('module' => 'ExternalLink', 'action' => 'redirect',
+					'url' => $url),
 						Piwik::isUserHasSomeViewAccess(),
 						$order = 6,
 						$isHTML = false,
 						$tooltip = false
 			);
+		}
 	}
 
 
@@ -78,11 +73,21 @@ class Piwik_ExternalLink extends Piwik_Plugin
 	 *
 	 * @param Piwik_Event_Notification $notification  notification object
 	 */
-	function getCssFiles( $notification )
+	public function getCssFiles( $notification )
 	{
 		$cssFiles = &$notification->getNotificationObject();
 
 		$cssFiles[] = "plugins/ExternalLink/templates/ExternalLink.css";
 	}
 
+
+	protected function getConfigLinks()
+	{
+		$config = Piwik_Config::getInstance()->ExternalLink;
+		if (isset($config['links']))
+		{
+			return $config['links'];
+		}
+		return array();
+	}
 }
